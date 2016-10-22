5b15a818b930ae9f1a0eab14a1f7b2f0ea1e58e2
Allen Kramer <kramer.alleng@gmail.com>
2016-10-22 12:02:18 -0400

    updated Ball.prototype.update to include ball animation

--- app/scripts/blong.js
+++ app/scripts/blong.js
@@ -1,10 +1,4 @@
-var animate = window.requestAnimationFrame ||
-        window.webkitRequestAnimationFrame ||
-        window.mozRequestAnimationFrame    ||
-        window.oRequestAnimationFrame      ||
-        window.msRequestAnimationFrame     ||
-        function(callback) { window.setTimeout(callback, 1000/60) };
-
+// BUILD CANVAS
 var canvas = document.getElementById('blongCanvas');
 var context = canvas.getContext('2d');
 var width = 1000;
@@ -12,17 +6,25 @@ var height = 600;
 canvas.width = width;
 canvas.height = height;
 
-// define variables for game elements
+// DEFINE VARIABLES FOR GAME ELEMENTS
 var player = new Player();
 var computer = new Computer();
 var ball = new Ball(width/2, height/2);
 
 
 
+// ANIMATE GAME
+
+var animate = window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame    ||
+        window.oRequestAnimationFrame      ||
+        window.msRequestAnimationFrame     ||
+        function(callback) { window.setTimeout(callback, 1000/60) };
+
 var update = function() {
     player.update();
-    ball.update();
-    // ball.update(player.paddle, computer.paddle);
+    ball.update(player.paddle, computer.paddle);
 };
 
 var step = function() {
@@ -53,12 +55,17 @@ window.onload = function() {
 
 
 
-// PADDLE FUNCTIONS
+/////////////////////////////////////////////////////////////////////////
+// PADDLE FUNCTIONS /////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
+
 function Paddle(x, y, width, height) {
     this.x = x;
     this.y = y;
     this.width = width;
     this.height = height;
+    this.x_speed = 0;
+    this.y_speed = 0;
 }
 
 Paddle.prototype.render = function() {
@@ -96,9 +103,9 @@ Player.prototype.update = function() {
     for(var key in keysDown) {
         var value = Number(key);
         if(value == 39) { // left arrow
-            this.paddle.move(0, -4);
+            this.paddle.move(0, -8);
         } else if (value == 37) { // right arrow
-            this.paddle.move(0, 4);
+            this.paddle.move(0, 8);
         } else {
             this.paddle.move(0, 0);
         }
@@ -124,13 +131,15 @@ Paddle.prototype.move = function(x, y) {
 
 
 
+/////////////////////////////////////////////////////////////////////////
+// BALL FUNCTIONS ///////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
 
-// BALL FUNCTIONS
 function Ball(x, y) {
   this.x = x;
   this.y = y;
   this.radius = 10;
-  this.x_speed = 2;
+  this.x_speed = 3;
   this.y_speed = 0;
 }
 
@@ -141,7 +150,43 @@ Ball.prototype.render = function() {
     context.fill();
 };
 
-Ball.prototype.update = function() {
+Ball.prototype.update = function(paddle1, paddle2) {
     this.x += this.x_speed;
     this.y += this.y_speed;
+    var top_x = this.x - 10;
+    var top_y = this.y - 10;
+    var bottom_x = this.x + 10;
+    var bottom_y = this.y + 10;
+
+    if (this.y - 10 < 0) { // hitting the top
+        this.y = 10;
+        this.y_speed = -this.y_speed;
+    } else if (this.y + 10 > 600) { // hitting the bottom
+        this.y = 590;
+        this.y_speed = -this.y_speed;
+    }
+
+    if (this.x < 0 || this.x > 1000) { // a point was scored, reset to beginnig position/speed
+        paddle1.y = (canvas.height - 125) / 2;
+        this.x_speed = 3;
+        this.y_speed = 0;
+        this.x = width/2;
+        this.y = height/2;
+    }
+
+    if (top_x > 500) {
+        if (top_x < (paddle1.x + paddle1.width) && bottom_x > paddle1.x && top_y < (paddle1.y + paddle1.height) && bottom_y > paddle1.y) {
+            // hit the player's paddle
+            this.x_speed = -3;
+            this.y_speed += (paddle1.y_speed / 2);
+            this.x += this.x_speed;
+        }
+    } else {
+        if(top_x < (paddle2.x + paddle2.width) && bottom_x > paddle2.x && top_y < (paddle2.y + paddle2.height) && bottom_y > paddle2.y) {
+          // hit the computer's paddle
+          this.x_speed = 3;
+          this.y_speed += (paddle2.y_speed / 2);
+          this.x += this.x_speed;
+        }
+    }
 };
